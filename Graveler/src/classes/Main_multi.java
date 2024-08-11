0e342485b407eb59d415892bf95547c764e47212
user <user@DESKTOP-DGR3D4D.fritz.box>
2024-08-11 18:17:35 +0200

    Git rid of unneeded import biginteger and added * 1 to be changed for larger amounts of runs

--- Graveler/src/classes/Main_multi.java
+++ Graveler/src/classes/Main_multi.java
@@ -3,7 +3,6 @@
  */
 package classes;
 
-import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -20,18 +19,18 @@ import java.util.concurrent.TimeUnit;
  * he used up all his 54 safe turns Then i count how many turns he was paralyzed
  * One will notice that this will lower the results as some of the better
  * attempts in reality would have failed already by having graveler explode and
- *  some of the calculated paralyzes would have happened after gravelers death in game.
+ * some of the calculated paralyzes would have happened after gravelers death in
+ * game.
  * 
  * for that attempt Also instead of just saving the highest number i will output
  * a more specific statistic
  * 
  * This is an alternative Attempt where i attempt to use multi-threading to cut
- * down time.
- * This works well as dividing the same code into 100 Tasks speeds the whole code up 
- * from ~255 seconds to around 24.24 seconds
+ * down time. This works well as dividing the same code into 100 Tasks speeds
+ * the whole code up from ~255 seconds to around 24.24 seconds
  * 
- * edit: i changed some types from integer to long to allow for more runs if needed
- * 		 this didn't change how long it took for 1000000000
+ * edit: i changed some types from integer to long to allow for more runs if
+ * needed this didn't change how long it took for 1000000000
  */
 public class Main_multi
 {
@@ -39,17 +38,18 @@ public class Main_multi
 	// Main method
 	public static void main(String[] args)
 	{
-		//Change run and task_number as needed but make sure run is dividable by task_number
-		
+		// Change run and task_number as needed but make sure run is dividable by
+		// task_number
+
 		// How many Runs should be done
-		long run = 1000000000;
-		
+		long run = 1000000000 * 1;	//Change the 1 if more Runs are needed
+
 		// In how many Tasks should the work be split
 		// In my case around 100 seemed to be a good sweetspot for 1000000000
 		int task_number = 100;
 
 		int max_value = 0;
-		
+
 		int attempts = 0;
 
 		// Result-Array that saves how many times how often he got paralyzed
@@ -58,13 +58,13 @@ public class Main_multi
 		ExecutorService executor = (ExecutorService) Executors.newFixedThreadPool(10);
 
 		List<Task> taskList = new ArrayList<>();
-		
+
 		// Measuring how long it takes
 		long start = System.currentTimeMillis();
-		
+
 		for (int i = 0; i < task_number; i++)
 		{
-			Task task = new Task(run/task_number);
+			Task task = new Task(run / task_number);
 			taskList.add(task);
 		}
 
@@ -81,23 +81,23 @@ public class Main_multi
 		}
 
 		executor.shutdown();
-		
-		//Combine Results
+
+		// Combine Results
 		for (int i = 0; i < resultList.size(); i++)
 		{
-			
+
 			// Get Result for a the Task
 			try
 			{
 				Future<int[]> future = resultList.get(i);
 				int[] result = future.get();
-				
+
 				// Add Result from Task to Full Result
 				for (int a = 0; a < 178; a += 1)
 				{
 					results[a] += result[a];
 				}
-				
+
 			}
 			catch (InterruptedException | ExecutionException e)
 			{
@@ -105,28 +105,26 @@ public class Main_multi
 			}
 		}
 
-
 		// output Results
 		for (int a = 0; a < 178; a += 1)
 		{
 
-
 			if (results[a] > 0)
 			{
 				max_value = a;
-				attempts = attempts + results[a] ;
+				attempts = attempts + results[a];
 			}
 
 			System.out.println(a + " times paralyzed: " + results[a] + " times");
 		}
 
 		System.out.println("The best attempt had " + max_value + " paralysed turns.");
-		
+
 		// How long did this Execution take?
 		long finish = System.currentTimeMillis();
-		long full_seconds = TimeUnit.MILLISECONDS.toSeconds(finish-start);
-		System.out.println("The Execution of " + run + " Runs took " + full_seconds + "." + ( finish-start-full_seconds ) + " Seconds." );
-		
+		long full_seconds = TimeUnit.MILLISECONDS.toSeconds(finish - start);
+		System.out.println("The Execution of " + run + " Runs took " + full_seconds + "."
+				+ (finish - start - full_seconds) + " Seconds.");
 
 	}
 
